d8dde68ee4241f551f62dba0e054048b4e5b8bd3
liuzhen <1390211724@qq.com>
2026-06-10 18:07:56 +0800

    deploy_files

--- deploy_github.sh
+++ deploy_github.sh
@@ -1,25 +1,17 @@
+# 在主仓库操作，不要进入 public 子目录
 git checkout files
 git add .
-git commit -m deploy_files
+git commit -m "deploy_files"
 git push -u origin files
 echo "已提交到GitHub!"
 
-sleep 2
-
-npx hexo g --watch
+# 生成静态文件
+npx hexo g
 echo "静态网站生成完毕！"
 
-sleep 2
+# 直接部署到 GitHub Pages（使用 hexo-deployer-git）
+npx hexo d
+echo "已部署到GitHub Pages!"
 
-cd public
-
-sleep 2
-
-git checkout master
-git add .
-git commit -m deploy_master
-git push -u origin master
-echo "已提交到GitHub!"
-
-echo 执行完成，按任意键结束
+echo "执行完成，按任意键结束"
 read -n 1
